06aad45f1c03a324d4009d479d7272d854e7d799
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-04-08 13:28:40 +0530

    search filters

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,19 +1,81 @@
 import prisma from '@/app/lib/util';
 
 export interface IListing{
-    userId ?: string
+    userId ?: string,
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 export default async function getListings( params : IListing ){
     try{
 
-        const {userId} = params;
+        const {userId,
+            roomCount, 
+            guestCount, 
+            bathroomCount, 
+            locationValue,
+            startDate,
+            endDate,
+            category,} = params;
 
         let query : any = {};
 
         if(userId){
             query.userId = userId
         }
+
+        if(category){
+            query.category = category
+        }   
+
+        // gte means greater than or equals , we have to put the '+' infront of roomcount
+        // so that can be converted to number, as we are passing it as a string
+        if(roomCount){
+            query.roomCount = {
+                gte : +roomCount
+            }
+        }
+
+        if(guestCount){
+            query.guestCount = {
+                gte : +guestCount
+            }
+        }
+
+        if(bathroomCount){
+            query.bathroomCount = {
+                gte : +bathroomCount
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+              reservations: {
+                some: {
+                  OR: [
+                    {
+                      endDate: { gte: startDate },
+                      startDate: { lte: startDate }
+                    },
+                    {
+                      startDate: { lte: endDate },
+                      endDate: { gte: endDate }
+                    }
+                  ]
+                }
+              }
+            }
+          }
+
 
         const listing = await prisma.listing.findMany({
             where : query,
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,12 +1,17 @@
 "use client"
 
+import useSearch from '@/app/hooks/SearchHook'
 import React from 'react'
 import {RiSearchLine} from 'react-icons/ri'
 import {VscSettings} from 'react-icons/vsc'
 
 const Search = () => {
+
+  const searchModal = useSearch();
+
   return (
-    <div className='flex w-screen md:max-w-max gap-5 justify-center items-center py-[5px] px-4 md:py-[6px] md:px-5 md:pr-1 rounded-full shadow-md border-[1px] md:absolute md:left-12 lg:static lg:ml-32 lg:py-2'>
+    <div onClick={searchModal.onOpen} 
+    className='flex w-screen md:max-w-max gap-5 justify-center items-center py-[5px] px-4 md:py-[6px] md:px-5 md:pr-1 rounded-full shadow-md border-[1px] md:absolute md:left-12 lg:static lg:ml-32 lg:py-2'>
                     <RiSearchLine className='text-xl md:hidden'/>
                     <div className='flex flex-col flex-grow-[4] md:flex-grow-0 md:flex-row md:items-center md:gap-3'>
                         <div className='font-[500] text-sm md:border-r md:border-slate-300 md:pr-4 lg:pr-5 lg:text-base'>Anywhere</div>
